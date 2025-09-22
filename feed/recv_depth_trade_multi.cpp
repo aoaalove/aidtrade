cee1617803b4c3ed92decf70f570d537e04ff588
aiden <aoaalove@gmail.com>
2025-09-22 13:54:22 +0800

    simplify feed

--- feed/recv_depth_trade_multi.cpp
+++ feed/recv_depth_trade_multi.cpp
@@ -9,6 +9,9 @@
 #include <chrono>
 #include <mutex>
 #include <format>
+#include <fstream>
+#include "parsearg.hpp"
+#include "spsc.hpp"
 
 using json = nlohmann::json;
 
@@ -16,6 +19,25 @@ using json = nlohmann::json;
 struct DepthBook {
 	std::vector<std::vector<std::string>> bids;
 	std::vector<std::vector<std::string>> asks;
+};
+
+struct Price {
+	std::string price;
+	std::string qty;
+};
+
+struct Depth {
+	Price	asks[5];
+	Price	bids[5];
+};
+
+struct RawData
+{
+	std::string symbol;
+	std::string type;	// depth, trade
+	std::string timestamp;
+	Price	trade;
+	Depth	depth; 	
 };
 
 std::unordered_map<std::string, DepthBook> depth_books;
@@ -43,11 +65,9 @@ void print_depth(const std::string& symbol, const DepthBook& book) {
 			<< std::setw(15) << std::setprecision(5) << std::right << bids_sorted[0][1]
 			<< std::endl;
 }
+
 void print_depth_all(const std::string& symbol, const DepthBook& book, 
 		size_t depth_count = 1) {
-	//  std::lock_guard<std::mutex> lock(depth_mtx);
-
-	// std::cout << "\033[H\033[J"; // 화면 clear
 	if (depth_count == 1) {
 		print_depth(symbol, book);
 		return;
@@ -82,112 +102,167 @@ void print_depth_all(const std::string& symbol, const DepthBook& book,
 	std::cout << "-----------------------------------------------------\n";
 }
 
-int main() {
-	ix::WebSocket ws;
-	ws.setUrl("wss://stream.binance.com:9443/stream");
+struct Config
+{
+	Config(nlohmann::json config)
+	{
+		url = config["binance"]["url"].get<std::string>();
+		print_depth = config["print_depth"].get<bool>();
+		print_trade = config["print_trade"].get<bool>();
+		depth_count = config["depth_count"].get<size_t>();	
+		for (const auto& item : config["binance"]["subscriptions"])
+		{
+			subs.push_back({std::move(item["symbol"].get<std::string>()),
+					std::move(item["streams"].get<std::vector<std::string>>())});
+		}
+	}
 
-	ws.setOnMessageCallback([&](const ix::WebSocketMessagePtr& msg) {
-			if (msg->type == ix::WebSocketMessageType::Open) {
-				std::cout << "✅ Connected to Binance multi-stream\n";
+	std::string url;
+	bool print_depth;
+	bool print_trade;
+	size_t	depth_count;
+	std::vector<std::pair<std::string, std::vector<std::string>>>	subs;
+};
 
-				// 여러 종목 depth + trade subscribe
-				json sub_msg = {
-					{"method", "SUBSCRIBE"},
-					{"params", 
-						{
-							"adausdt@trade", "adausdt@depth5@100ms",
-							"btcusdt@trade", "btcusdt@depth5@100ms",
-							"ethusdt@trade", "ethusdt@depth5@100ms"
-						}
-					},
-					{"id", 1}
-				};
-				ws.send(sub_msg.dump());
-			} else if (msg->type == ix::WebSocketMessageType::Message) {
-				try {
-					auto js = json::parse(msg->str);
+void operOpen(ix::WebSocket& ws, const Config& config)
+{
+	std::cout << "[O]Connected to Binance multi-stream\n";
 
-					if (!js.contains("stream") || !js.contains("data"))
-						return;
+	// 여러 종목 depth + trade subscribe
+	json sub_msg = {
+		{"method", "SUBSCRIBE"},
+		{"params", 
+			{
+			}
+		},
+		{"id", 1}
+	};
+	for (const auto& sub : config.subs)
+	{
+		for (const auto& stream : sub.second)
+			sub_msg["params"].push_back(sub.first + "@" + stream); 
+	}
 
-					std::string stream = js["stream"];
-					size_t	pos = stream.find('@');
-					std::string symbol = stream.substr(0, pos);
-					stream = stream.substr(pos+1);
-					auto data = js["data"];
-					//std::cout << data.dump(4) << std::endl;
+	ws.send(sub_msg.dump());
+}
 
+void operMessage(const ix::WebSocketMessagePtr& msg, const Config& config)
+{
+	try {
+		std::lock_guard<std::mutex> lock(depth_mtx);
+		auto js = json::parse(msg->str);
 
-					// ---------------- Depth 업데이트 ----------------
-					if (stream == "depth5@100ms")  {
-						// event == "depthUpdate" or (data.contains("bids") && data.contains("asks"))) 
-						std::lock_guard<std::mutex> lock(depth_mtx);
+		if (!js.contains("stream") || !js.contains("data"))
+			return;
 
-						auto& book = depth_books[symbol];
+		std::string stream = js["stream"];
+		size_t	pos = stream.find('@');
+		std::string symbol = stream.substr(0, pos);
+		stream = stream.substr(pos+1);
+		auto data = js["data"];
+		// ---------------- Depth update ----------------
+		if (stream == "depth5@100ms")  {
 
-						auto bids_update = data.value("bids", std::vector<std::vector<std::string>>{});
-						auto asks_update = data.value("asks", std::vector<std::vector<std::string>>{});
+			auto& book = depth_books[symbol];
 
-						// Bid 업데이트
-						for (auto& bid : bids_update) {
-							auto& price = bid[0];
-							auto& qty = bid[1];
-							auto it = std::find_if(book.bids.begin(), book.bids.end(),
-									[&](auto& x) { return x[0] == price; });
-							if (it != book.bids.end()) {
-								if (qty != "0") (*it)[1] = qty;
-								else book.bids.erase(it);
-							} else if (qty != "0") {
-								book.bids.push_back({price, qty});
-							}
-						}
+			auto bids_update = data.value("bids", 
+					std::vector<std::vector<std::string>>{});
+			auto asks_update = data.value("asks", 
+					std::vector<std::vector<std::string>>{});
 
-						// Ask 업데이트
-						for (auto& ask : asks_update) {
-							auto& price = ask[0];
-							auto& qty = ask[1];
-							auto it = std::find_if(book.asks.begin(), book.asks.end(),
-									[&](auto& x) { return x[0] == price; });
-							if (it != book.asks.end()) {
-								if (qty != "0") (*it)[1] = qty;
-								else book.asks.erase(it);
-							} else if (qty != "0") {
-								book.asks.push_back({price, qty});
-							}
-						}
-
-						print_depth_all(symbol, book, 5);
-					} else {
-						std::string symbol2 = data.value("s", "");
-						std::string event  = data.value("e", "");
-						if (event == "trade") {
-							std::string price = data.value("p", "");
-							std::string qty   = data.value("q", "");
-							bool isBuyerMaker = data.value("m", false);
-
-							std::cout << "[TRADE] " << symbol
-								<< " P: " << price
-								<< " Q: "   << qty
-								<< " : " << (isBuyerMaker ? "SELL" : "BUY")
-								<< std::endl;
-						}
-					}
-					// ---------------- Trade ----------------
-				} catch (const std::exception& e) {
-					std::cerr << "❌ JSON parse error: " << e.what() << std::endl;
+			// Bid update
+			for (auto& bid : bids_update) {
+				auto& price = bid[0];
+				auto& qty = bid[1];
+				auto it = std::find_if(book.bids.begin(), book.bids.end(),
+						[&](auto& x) { return x[0] == price; });
+				if (it != book.bids.end()) {
+					if (qty != "0") (*it)[1] = qty;
+					else book.bids.erase(it);
+				} else if (qty != "0") {
+					book.bids.push_back({price, qty});
 				}
 			}
-			else if (msg->type == ix::WebSocketMessageType::Error) {
-				std::cerr << "❌ WebSocket Error: " << msg->errorInfo.reason << std::endl;
+
+			// Ask update
+			for (auto& ask : asks_update) {
+				auto& price = ask[0];
+				auto& qty = ask[1];
+				auto it = std::find_if(book.asks.begin(), book.asks.end(),
+						[&](auto& x) { return x[0] == price; });
+				if (it != book.asks.end()) {
+					if (qty != "0") (*it)[1] = qty;
+					else book.asks.erase(it);
+				} else if (qty != "0") {
+					book.asks.push_back({price, qty});
+				}
 			}
-			});
 
-			ws.start();
+			if (config.print_depth)
+				print_depth_all(symbol, book, config.depth_count);
+		} else {
+			std::string symbol2 = data.value("s", "");
+			std::string event  = data.value("e", "");
+			if (event == "trade") {
+				std::string price = data.value("p", "");
+				std::string qty   = data.value("q", "");
+				bool isBuyerMaker = data.value("m", false);
 
-			// 메인 쓰레드 keep alive
-			while (true)
-				std::this_thread::sleep_for(std::chrono::seconds(1));
-
-			return 0;
+				if (config.print_trade)
+				{
+					std::cout << "[TRADE] " << symbol
+						<< " P: " << price
+						<< " Q: "   << qty
+						<< " : " << (isBuyerMaker ? "SELL" : "BUY")
+						<< std::endl;
+				}
+			}
+		}
+		// ---------------- Trade ----------------
+	} catch (const std::exception& e) {
+		std::cerr << "[X]JSON parse error: " << e.what() << std::endl;
 	}
+}
+
+int main(int argc, char* argv[]) {
+	// parse arguments
+	ParseArg pa(argc, argv);
+
+	// read config.json
+	std::string config_file = "./config.json";
+	std::fstream	fs(config_file);
+	if (!fs.is_open()) {
+		std::cout << std::format("{} file not found", config_file);	
+		return -1;
+	}
+	nlohmann::json js_config;
+	fs >> js_config;
+	Config config(js_config);	
+	ix::WebSocket ws;
+	ws.setUrl(config.url);
+
+	ws.setOnMessageCallback([&](const ix::WebSocketMessagePtr& msg) {
+			switch (msg->type)
+			{
+				case ix::WebSocketMessageType::Open:
+					operOpen(ws, config);
+				break;
+				case ix::WebSocketMessageType::Message:
+					operMessage(msg, config);
+				break;
+				case ix::WebSocketMessageType::Error:
+					std::cerr << "[X]WebSocket Error: " << msg->errorInfo.reason << std::endl;
+				break;
+				default:
+				break;
+			}
+	});
+
+	ws.start();
+
+	while (true)
+		std::this_thread::sleep_for(std::chrono::seconds(1));
+
+	return 0;
+}
 
--- feed/recv_depth_trade_multi.d
+++ feed/recv_depth_trade_multi.d
@@ -1,1 +1,5 @@
-recv_depth_trade_multi: recv_depth_trade_multi.cpp
+recv_depth_trade_multi: recv_depth_trade_multi.cpp \
+ /home/aiden/projects/aidtrade/include/parsearg.hpp \
+ /home/aiden/projects/aidtrade/include/spsc.hpp
+/home/aiden/projects/aidtrade/include/parsearg.hpp:
+/home/aiden/projects/aidtrade/include/spsc.hpp:
